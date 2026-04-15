ea1661b2fe3f09a0f7e21e21808a3457c00e772f
Javhuila <jeferson_valbuenahu@fet.edu.co>
2026-04-14 23:45:49 -0500

    Sarita TQM

---
+++ backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/modelo/Dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package com.project.flutter_backend_desktop.modelo.Dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+    private String codigo;
+    private String nombre;
+    private String contenido;
+    private Double precio;
+    private String marca;
+    private String fotoUrl;
+
+    private Integer categoriaId;
+    private Integer clasificacionId;
+}
