b9ea56ba5aed6efcb57011ad801b29fbbb13b297
Javhuila <jeferson_valbuenahu@fet.edu.co>
2026-04-14 22:39:12 -0500

    Widget Client "only view"

---
+++ frontend/flutter_application_desktop/lib/components/Clientes/add_cliente.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class AddCliente extends StatefulWidget {
+  const AddCliente({super.key});
+
+  @override
+  State<AddCliente> createState() => _AddClienteState();
+}
+
+class _AddClienteState extends State<AddCliente> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Agregar cliente"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person_add_alt_outlined),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  spacing: 5,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Nombre"),
+                        suffixIcon: Icon(Icons.location_history),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Apellido"),
+                        suffixIcon: Icon(Icons.location_history),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Telefono"),
+                        suffixIcon: Icon(Icons.phonelink_ring_rounded),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Direccion"),
+                        suffixIcon: Icon(Icons.display_settings_rounded),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Barrio/Sector"),
+                        suffixIcon: Icon(Icons.other_houses_rounded),
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Correo"),
+                        suffixIcon: Icon(Icons.alternate_email_rounded),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 250,
+                      height: 250,
+                      color: Colors.white,
+                      margin: EdgeInsets.all(3),
+                      child: Center(child: Icon(Icons.add_a_photo_rounded)),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- frontend/flutter_application_desktop/lib/components/Clientes/cliente.dart
+++ frontend/flutter_application_desktop/lib/components/Clientes/cliente.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_desktop/components/Clientes/add_cliente.dart';
 
 class Cliente extends StatefulWidget {
   const Cliente({super.key});
@@ -8,11 +9,102 @@ class Cliente extends StatefulWidget {
 }
 
 class _ClienteState extends State<Cliente> {
+  final _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Form(child: Column(children: [])),
+      appBar: AppBar(
+        title: const Text('Productos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddCliente()),
+              );
+            },
+            icon: Icon(Icons.person_add_alt_1_outlined),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _searchController,
+                keyboardType: TextInputType.name,
+                style: const TextStyle(
+                  fontSize: 20,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                decoration: InputDecoration(
+                  labelText: "Buscar",
+                  hintText: "Buscar clientes",
+                  suffixIcon: const Icon(Icons.search_outlined, size: 40),
+                  suffixIconColor: Colors.grey,
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  border: InputBorder.none,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: Colors.grey),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: Colors.grey),
+                    gapPadding: 10,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 20,
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: Colors.red, width: 2.0),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(
+                      color: Colors.deepOrange,
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Form(
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 15),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          child: Column(
+                            spacing: 9,
+                            children: [
+                              Text("Nombre cliente"),
+                              Text("telefono"),
+                              Text("Barrio"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
