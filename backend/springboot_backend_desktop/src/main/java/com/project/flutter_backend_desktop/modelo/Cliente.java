bc3dcc19e1cd14a1a5288c94d0490a7fbf81373b
Javhuila <jeferson_valbuenahu@fet.edu.co>
2026-04-21 12:11:04 -0500

    Fix Cliente

--- backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/modelo/Cliente.java
+++ backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/modelo/Cliente.java
@@ -1,9 +1,11 @@
 package com.project.flutter_backend_desktop.modelo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,14 +16,20 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer idCliente;
     String nombreCompleto;
+
+    @NotBlank(message = "El nombre es obligatorio")
     String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
     String apellido;
+    Integer cedula;
     String correo;
     String telefono;
 }
--- backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/repositorio/ClienteRepositorio.java
+++ backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/repositorio/ClienteRepositorio.java
@@ -1,11 +1,24 @@
 package com.project.flutter_backend_desktop.repositorio;
 
 import com.project.flutter_backend_desktop.modelo.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepositorio extends JpaRepository<Cliente, Integer>,
+        JpaSpecificationExecutor<Cliente> {
 
-    List<Cliente> findByNombre(String nombre);
+    @Query(value = "select cedula from cliente where id_cliente=" +
+            "(select max(id_cliente) from cliente);",
+            nativeQuery = true)
+    Integer buscarCedula();
+
+    List<Cliente> findTop10ByNombreCompletoContainingIgnoreCase(String nombreCompleto);
+
+    Page<Cliente> findAll(Specification<Cliente> spec, Pageable pageable);
 }
