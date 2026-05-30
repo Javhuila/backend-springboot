6e662f2e841464504dfa070b811f9d2f1528255a
Javhuila <jeferson_valbuenahu@fet.edu.co>
2026-05-29 22:27:57 -0500

    Add entity Pago for Venta

---
+++ backend/springboot_backend_desktop/src/main/java/com/project/flutter_backend_desktop/modelo/Pago.java
@@ -0,0 +1,54 @@
+package com.project.flutter_backend_desktop.modelo;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.flutter_backend_desktop.modelo.Enum.TipoTarjeta;
+import com.project.flutter_backend_desktop.modelo.Enum.TipoPago;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "venta")
+@Data
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPago;
+
+    @ManyToOne
+    @JoinColumn(name = "venta_id")
+    @JsonIgnore
+    @JsonBackReference
+    private Venta venta;
+
+    @Enumerated(EnumType.STRING)
+    private TipoPago tipoPago;
+
+    @Column(nullable = false)
+    private Double monto;
+
+    private String banco;
+
+    private String numeroReferencia;
+
+    @Enumerated(EnumType.STRING)
+    private TipoTarjeta tipoTarjeta;
+
+    private String ultimosDigitos;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaPago;
+
+    private String comprobante;
+
+    private String entidadFinanciera;
+
+    @PrePersist
+    public void prePersist() {
+        fechaPago = LocalDateTime.now();
+    }
+}
